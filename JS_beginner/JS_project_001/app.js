f1ba39af98ea2d025048871633763e433f8cb1d6
dikovinki <andrey.zhelkovsky@gmail.com>
2020-09-07 17:36:25 -0400

    adding first javascript project

---
+++ JS_beginner/JS_project_001/app.js
@@ -0,0 +1,10 @@
+// 1. variables
+
+const firstName = 'Andrey' // data_type STRING
+const lastName = 'Zhelkovsky'  // data_type STRING
+let age = 41   // data_type NUMBER
+const isProgrammer = true // data_type boolean
+
+age = 42
+
+console.log(age)
